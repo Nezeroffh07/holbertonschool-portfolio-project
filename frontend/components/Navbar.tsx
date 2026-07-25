b81f62fe974183e0b41f00b23d52d734ad69b87d
Sahila001 <sahilamikayilova001@gmail.com>
2026-07-25 17:30:09 +0400

    Add frontend components

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>TUP</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Find Team</li>
+        <li>About</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+}
