185d381fdfdbbb2c4cbb92241f0571a559ec91e5
Nezerofh07 <nzrovnurlan3@gmail.com>
2026-08-12 23:37:55 +0400

    Update api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,1 +1,11 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+
+export function getAuthHeaders(): HeadersInit {
+  const token = localStorage.getItem("access_token");
+  return token
+    ? {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      }
+    : { "Content-Type": "application/json" };
+}
